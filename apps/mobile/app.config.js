c3cd5051a7da30377607cc3ea25938e8f3e03305
kisuwo16-5108 <kisuwo16-5108@users.noreply.github.com>
2026-07-20 08:50:41 +0900

    Fix iOS app config copy

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,13 +1,17 @@
 const apiBaseUrl =
   process.env.EXPO_PUBLIC_API_BASE_URL ?? "https://pf-two-eta.vercel.app";
 
+const photoPermissionMessage =
+  "반려동물 건강 기록에 보호자가 선택한 사진과 영상을 첨부하기 위해 사진 보관함 접근 권한을 사용합니다.";
+
 module.exports = {
   expo: {
     name: "PetFlow",
     slug: "petflow-mobile",
-    description: "보호자 관찰을 병원에 전달하기 좋은 흐름으로 정리하는 PetFlow 모바일 앱",
+    description:
+      "보호자 관찰을 병원에 전달하기 좋은 흐름으로 정리하는 PetFlow 모바일 앱입니다.",
     scheme: "petflow",
-    version: "0.1.0",
+    version: "1.0",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
     splash: {
@@ -27,8 +31,7 @@ module.exports = {
         {
           cameraPermission: false,
           microphonePermission: false,
-          photosPermission:
-            "반려동물 건강 기록에 보호자가 선택한 사진과 영상을 첨부하기 위해 사진 보관함 접근 권한을 사용합니다.",
+          photosPermission: photoPermissionMessage,
         },
       ],
     ],
@@ -37,6 +40,7 @@ module.exports = {
       supportsTablet: false,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
+        NSPhotoLibraryUsageDescription: photoPermissionMessage,
       },
     },
     android: {
